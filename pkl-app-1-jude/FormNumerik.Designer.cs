4229d839f218fbb3fb952ae40356ff72336d4401
druryyl <drury_yl@yahoo.com>
2023-03-29 10:42:21 +0700

    Proses Pembagian - Tipe Data Double

--- pkl-app-1-jude/FormNumerik.Designer.cs
+++ pkl-app-1-jude/FormNumerik.Designer.cs
@@ -36,6 +36,8 @@
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -70,7 +72,7 @@
             // 
             this.textBox3.Location = new System.Drawing.Point(81, 220);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.Size = new System.Drawing.Size(178, 20);
             this.textBox3.TabIndex = 6;
             this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
@@ -85,7 +87,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(127, 166);
+            this.button2.Location = new System.Drawing.Point(173, 166);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(40, 23);
             this.button2.TabIndex = 7;
@@ -97,7 +99,7 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(81, 86);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(178, 20);
             this.textBox1.TabIndex = 8;
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
@@ -105,15 +107,37 @@
             // 
             this.textBox2.Location = new System.Drawing.Point(81, 130);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(178, 20);
             this.textBox2.TabIndex = 9;
             this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(127, 166);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(40, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(219, 166);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(40, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "/";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // FormNumerik
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(351, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
@@ -139,5 +163,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
--- pkl-app-1-jude/FormNumerik.cs
+++ pkl-app-1-jude/FormNumerik.cs
@@ -33,5 +33,21 @@ namespace pkl_app_1_jude
             var hasil = data1 * data2;
             textBox3.Text = Convert.ToString(hasil);
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var data1 = Convert.ToInt64(textBox1.Text);
+            var data2 = Convert.ToInt64(textBox2.Text);
+            var hasil = data1 - data2;
+            textBox3.Text = Convert.ToString(hasil);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var data1 = Convert.ToDouble(textBox1.Text);
+            var data2 = Convert.ToDouble(textBox2.Text);
+            var hasil = data1 / data2;
+            textBox3.Text = hasil.ToString("0.##");
+        }
     }
 }
